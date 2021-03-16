6316ae77cfa8855883597133c4b697927aa49d91
jenny sager <jennysager@jennys-mbp.attlocal.net>
2021-03-15 20:12:04 -0400

    First commit

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+/* Javascript File */
+
+document.writeln("<a href='default.html'>Home</a> | <a href='page1.html'>Makeup</a> | <a href='page2.html'>Hair</a> | <a href='page3.html'>Nails</a> | <a href='page4.html'>Skincare</a>");
